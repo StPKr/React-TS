7dd01bf8b3f6b51a4287472a01b28899406d7e2b
StPKr <sotfuni25@gmail.com>
2025-03-03 01:21:31 +0200

    click and form elements event types

--- src/Person.tsx
+++ src/Person.tsx
@@ -9,14 +9,29 @@ interface Person {
 export const Person = (props: Person) => {
     const [isShowInfo, setShowInfo] = useState<boolean | null>(false);
 
+    const [personBio, setPersonBio] = useState<string | null>(null);
+
     const toggleInfo = () => {
         setShowInfo((prev) => !prev)
     };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => { //*event needs a predefined react element and it needs the type of element in this case input as type
+        setPersonBio(event.target.value);
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => { //*example for form elements/events
+        event.preventDefault();
+
+    }
+
     return <div>
-        {isShowInfo && (
+        {isShowInfo && (<>
             <p>
                 Name: {props.name}  Age: {props.age} <br></br> This person {props.isMarried ? "is married" : "is single"}
             </p>
+            <p>{props.name} Bio: {personBio === null ? "No Bio available" : personBio}</p>
+            <input onChange={handleChange} />
+        </>
         )}
         <button onClick={toggleInfo}>Toggle Info</button>
     </div>
